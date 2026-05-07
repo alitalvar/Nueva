84ca9599de37cc1398f5ec53fbec635fdb99c222
alitalvar <alitalvar96@gmail.com>
2026-05-07 13:00:01 +0530

    json

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  tsr: {
-    appDirectory: "src",
-  },
   server: {
     preset: "vercel",
   },
   vite: {
-    plugins: [tailwindcss(), tsConfigPaths()],
+    plugins: [
+      tailwindcss(),
+      tsConfigPaths(),
+    ],
     build: {
       chunkSizeWarningLimit: 2000,
     },
